1f747ce81b7c3acb912f82b9b3eca4f8bdaa56df
Yuriy <yuriy@harmony.one>
2023-02-10 23:52:52 +0300

    icons fixes

--- src/pages/Stream/Footer/index.tsx
+++ src/pages/Stream/Footer/index.tsx
@@ -10,9 +10,21 @@ import { useStores } from "stores";
 export const StreamFooter = observer(() => {
     const { stream } = useStores();
 
-    return <Box direction="row" gap="20px">
-        <div onClick={() => stream.likeLiveStream()}>
-            <AiOutlineHeart color="#38b3ff" />
+    return <Box
+        direction="row"
+        gap="30px"
+        fill={true}
+        justify="end"
+        align="center"
+        pad={{ horizontal: 'large' }}
+    >
+        <Box
+            onClick={() => stream.likeLiveStream()}
+            align="center"
+            direction="row"
+            style={{ cursor: 'pointer' }}
+        >
+            <AiOutlineHeart color="#38b3ff" size="24px" />
             <span style={{
                 color: '#38b3ff',
                 margin: '0 0 1px 5px',
@@ -20,14 +32,14 @@ export const StreamFooter = observer(() => {
             }}>
                 {stream.data.totalLikes}
             </span>
+        </Box>
+
+        <div onClick={() => { }} style={{ cursor: 'pointer' }}>
+            <BiDonateHeart color="#38b3ff" size="24px" />
         </div>
 
-        <div onClick={() => {}}>
-            <BiDonateHeart color="#38b3ff" />
-        </div>
-
-        <div onClick={() => stream.toggleChatVisible()}>
-            <BiChat color="#38b3ff" />
+        <div onClick={() => stream.toggleChatVisible()} style={{ cursor: 'pointer' }}>
+            <BiChat color="#38b3ff" size="24px" />
         </div>
     </Box>
 })
